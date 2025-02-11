5fa6207e7bd020ce158c71e2bdf226421989c521
Rhynne Gracelle <196156994+alifirocodes@users.noreply.github.com>
2025-02-12 03:37:50 +0800

    Add validation and relational logic to views.py

--- connectly_project/posts/views.py
+++ connectly_project/posts/views.py
@@ -5,6 +5,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import User
 
+# Validation and relational logic 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User, Post, Comment
+from .serializers import UserSerializer, PostSerializer, CommentSerializer
+
+
 
 # Create your views here.
 def get_users(request):
@@ -48,4 +56,50 @@ def delete_user(request, id):
             return JsonResponse({'message': 'User deleted successfully'}, status=200)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
+
+
+# Validation and relational logic 
+class UserListCreate(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostListCreate(APIView):
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentListCreate(APIView):
+    def get(self, request):
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
